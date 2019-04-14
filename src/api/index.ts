e4261a080c35c3168ec076231f46b48f8ddf695b
1998MaxChykalo <maxymchykalo@gmail.com>
2019-04-14 09:41:03 +0300

    Move api calls logic to ApiService

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,25 @@
+
 import Mapper, { MapFunction } from "../mappers/Mapper";
 import Answer from "../models/Answer";
 import User from "../models/User";
 
 const API_URL = 'http://localhost:3000';
 
+const toJson = (res: Response) => res.json();
+const handleErrors = (res: Response) => {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res;
+}
+export const fetch = <T>(endpoint: string) =>
+  (cb: MapFunction<T>) =>
+    () => window.fetch(`${API_URL}/${endpoint}`)
+            .then(handleErrors)
+            .then(toJson)
+            .then(cb);
 
-export const fetch = <T>(endpoint: string) => (cb: MapFunction<T>) => () => window.fetch(`${API_URL}/${endpoint}`).then(res => res.json()).then(cb);
-export const fetchAnswers = fetch<Answer>('answers.1.json')(Mapper.toAnswers);
-export const fetchUsers = fetch<User>('users.json')(Mapper.toUsers);
-
+export class ApiService {
+  static fetchAnswers = fetch<Answer>("answers.1.json")(Mapper.toAnswers);
+  static fetchUsers = fetch<User>("users.json")(Mapper.toUsers);
+}
